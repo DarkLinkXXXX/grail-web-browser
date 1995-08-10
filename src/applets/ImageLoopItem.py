890b45ff464d24b40c20a5113738930149743965
guido <guido>
1995-08-10 17:56:33 +0000

    Emulate Java's ImageLoopItem applet class

---
+++ src/applets/ImageLoopItem.py
@@ -0,0 +1,41 @@
+# Java compatibility hack
+
+from Tkinter import *
+import urllib
+import time
+
+class ImageLoopItem:
+
+    def __init__(self, master, img='doc:/demo/images/duke/',
+		 pause=3900, align=None, **kw):
+	self.master = master
+	self.pause = pause
+	if img[:4] == 'doc:':
+	    img = 'http://monty.cnri.reston.va.us/grail' + img[4:]
+	self.browser = master.grail_browser
+	self.images = []
+	i = 0
+	while 1:
+	    i = i+1
+	    url = "%s/T%s.gif" % (img, i)
+	    image = self.browser.get_image(url)
+	    if not image: break
+	    self.images.append(image)
+	self.label = Label(master, text=url)
+	self.label.pack()
+	self.index = 0
+	if self.images:
+	    self.update()
+
+    def update(self):
+	if self.index >= len(self.images):
+	    delay = 100 + self.pause
+	    self.index = 0
+	else:
+	    delay = 100
+	try:
+	    self.label['image'] = self.images[self.index]
+	    self.index = self.index + 1
+	    self.master.tk.createtimerhandler(delay, self.update)
+	except:
+	    pass
