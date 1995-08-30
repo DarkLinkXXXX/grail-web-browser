47fa9b66028cf786c11ea15f2985ee8e5ed58250
guido <guido>
1995-08-30 15:01:14 +0000

    made it a little nicer

--- src/protocols/docAPI.py
+++ src/protocols/docAPI.py
@@ -1,5 +1,7 @@
+"""doc: URL scheme handler."""
+
 import urllib
 
-def open_doc(url):
-    if url[:1] != '/': url = '/' + url
-    return urllib.urlopen("http://monty.cnri.reston.va.us/grail" + url)
+def open_doc(parturl):
+    if parturl[:1] != '/': parturl = '/' + parturl
+    return urllib.urlopen("http://monty.cnri.reston.va.us/grail" + parturl)
