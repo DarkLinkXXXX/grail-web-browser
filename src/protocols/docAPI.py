17ec95287b9ae9b039d9127bc896eee5f62530b5
guido <guido>
1995-08-30 21:16:34 +0000

    Don't use urllib.urlopen(); generate 302 error instead

--- src/protocols/docAPI.py
+++ src/protocols/docAPI.py
@@ -1,7 +1,6 @@
 """doc: URL scheme handler."""
 
-import urllib
-
 def open_doc(parturl):
     if parturl[:1] != '/': parturl = '/' + parturl
-    return urllib.urlopen("http://monty.cnri.reston.va.us/grail" + parturl)
+    newurl = "http://monty.cnri.reston.va.us/grail" + parturl
+    raise IOError, ('redirect error', 302, 'Moved', {'location': newurl})
