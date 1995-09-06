cfa5558102ad4ef2cefe2976272a3df1d2eb431e
guido <guido>
1995-09-06 20:17:40 +0000

    Reformatted using 4-space indents

--- src/protocols/nullAPI.py
+++ src/protocols/nullAPI.py
@@ -4,26 +4,26 @@ META, DATA, DONE = 'META', 'DATA', 'DONE'
 
 class null_access:
 
-	def __init__(self, url, method, params):
-		self.state = META
-	
-	def pollmeta(self):
-		assert(self.state == META)
-		return "Ready", 1
-	
-	def getmeta(self):
-		assert(self.state == META)
-		self.state = DATA
-		return 204, "No content", {}
-	
-	def polldata(self):
-		assert(self.state == DATA)
-		return "Ready", 1
-	
-	def getdata(self, maxbytes):
-		assert(self.state == DATA)
-		self.state = DONE
-		return ""
+    def __init__(self, url, method, params):
+	self.state = META
 
-	def close(self):
-		pass
+    def pollmeta(self):
+	assert(self.state == META)
+	return "Ready", 1
+
+    def getmeta(self):
+	assert(self.state == META)
+	self.state = DATA
+	return 204, "No content", {}
+
+    def polldata(self):
+	assert(self.state == DATA)
+	return "Ready", 1
+
+    def getdata(self, maxbytes):
+	assert(self.state == DATA)
+	self.state = DONE
+	return ""
+
+    def close(self):
+	pass
