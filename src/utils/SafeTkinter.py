6e1da10ac8ed8e4031275671030f9a79bd644246
guido <guido>
1995-09-06 20:05:30 +0000

    Import the constants from (new) standard module Tkconstants

--- src/utils/SafeTkinter.py
+++ src/utils/SafeTkinter.py
@@ -10,65 +10,7 @@ from Tkinter import CallableTypes, TkVersion, TclVersion, \
      Scale, Scrollbar, Text, Image, PhotoImage, BitmapImage, \
      OptionMenu, \
      image_names, image_types
-
-# Symbolic constants (easier copied than included)
-
-# Booleans
-NO=FALSE=OFF=0
-YES=TRUE=ON=1
-
-# -anchor
-N='n'
-S='s'
-W='w'
-E='e'
-NW='nw'
-SW='sw'
-NE='ne'
-SE='se'
-CENTER='center'
-
-# -fill
-NONE='none'
-X='x'
-Y='y'
-BOTH='both'
-
-# -side
-LEFT='left'
-TOP='top'
-RIGHT='right'
-BOTTOM='bottom'
-
-# -relief
-RAISED='raised'
-SUNKEN='sunken'
-FLAT='flat'
-RIDGE='ridge'
-GROOVE='groove'
-
-# -orient
-HORIZONTAL='horizontal'
-VERTICAL='vertical'
-
-# -tabs
-NUMERIC='numeric'
-
-# -wrap
-CHAR='char'
-WORD='word'
-
-# -align
-BASELINE='baseline'
-
-# Special tags, marks and insert positions
-SEL='sel'
-SEL_FIRST='sel.first'
-SEL_LAST='sel.last'
-END='end'
-INSERT='insert'
-CURRENT='current'
-ANCHOR='anchor'
+from Tkconstants import *
 
 def _castrate(tk):
     """Remove all Tcl commands that can affect the file system."""
