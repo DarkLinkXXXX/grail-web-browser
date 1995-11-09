72c1088be19e90b9d042eeab161b0833ea655f44
guido <guido>
1995-11-09 17:25:26 +0000

    Point to grail-0.2 tree by default.

--- src/protocols/docAPI.py
+++ src/protocols/docAPI.py
@@ -7,7 +7,7 @@ class doc_access(null_access):
     def __init__(self, url, method, params):
 	null_access.__init__(self, url, method, params)
 	if url[:1] != '/': url = '/' + url
-	self.url = "http://monty.cnri.reston.va.us/grail" + url
+	self.url = "http://monty.cnri.reston.va.us/grail-0.2" + url
 
     def getmeta(self):
 	null_access.getmeta(self)	# assert, state change
