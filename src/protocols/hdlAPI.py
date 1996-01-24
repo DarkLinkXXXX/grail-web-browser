b8e844959a0f3a299f5b63765ea4ef6c2a50f46d
guido <guido>
1996-01-24 22:09:09 +0000

    When a handle resolves to multiple URLs, display some HTML to let the user choose.  Discard the "chooser" mechanism, the error handler, and some other oddities.  Reformulated and added some restrictions.

--- src/protocols/hdlAPI.py
+++ src/protocols/hdlAPI.py
@@ -4,9 +4,11 @@ This module glues the backend CNRI handle client resolution module in
 hdllib.py to the Grail 0.2 URI protocol API.  Currently we are only
 interested in URL type handles, so we can get away semantically with
 returning an HTTP-style relocation error to the browser for the
-resolved handle.
+resolved handle (if the handle resolves to a single URL), or to
+generating a piece of HTML which lets the user choose one (if it
+resolves to multiple URLs).
 
-Problems as of 25-Oct-1995:
+XXX Problems:
 
 	1. hdllib.py currently only supports the older handle
 	   protocol.  Specifically, it doesn't support dynamic
@@ -14,12 +16,13 @@ Problems as of 25-Oct-1995:
 	   support local handle servers.  This isn't a big deal
 	   currently because no local handle servers have been
 	   deployed, and the global system is backward compatible with
-	   the old protocol.
+	   the old protocol.  (We aren't even entirely sure whether
+	   the protocol has actually changed.)
 
 	2. I don't think it handles continuation packets correctly.
 	   This isn't a big deal since in this context, we're only
 	   concerned with URL type values, which are smaller than the
-	   packet size.
+	   packet size.  (This is really an issue with hdllib.py.)
 
 	3. Issuing a 302 relocate isn't the proper thing to do in the
 	   long term, because it forces the user back into URL space.
@@ -30,6 +33,11 @@ Problems as of 25-Oct-1995:
 	   with the CNRI handle group to define this and we'll be
 	   experimenting with solutions in the future.  This should be
 	   good enough for now.
+
+	4. Handle resolution is done synchronous, thereby defeating
+	   the intended asynchronous API.  This should be fixed by
+	   adding an asynchronous interface to hdllib.py.
+
 """
 
 import hdllib
@@ -41,24 +49,31 @@ import nullAPI
 HANDLE_TYPES = [hdllib.HDL_TYPE_URL]
 
 
-
-class HandleResolutionChooser:
-    """Abstract class implementing selection of a single handle
-    resolution from a list of resolutions.  A handle can actually
-    resolve to multiple URLs and we need a way to select between them.
-    The default (this class) is to simply choose one randomly out of
-    the list.  Other selection mechanisms can include generating an
-    HTML page containing all the links, or popping up a GUI selection
-    list dialog and letting the user choose.
+# HTML boilerplate for response on handle with multiple URLs
+HTML_HEADER = """<HTML>
 
-    Methods to Override:
+<HEAD>
+<TITLE>Multiple URLs From Handle</TITLE>
+</HEAD>
 
-       choose(handle_resolution_list)  -- returns one item from the list
-    """
-    def choose(self, handle_resolution_list):
-	import rand
-	return rand.choice(handle_resolution_list)
-    
+<BODY>
+
+<H1>Multiple URLs</H1>
+
+The handle you have selected resolves to multiple URLs.
+Please select one from the following list:
+
+<UL>
+"""
+
+HTML_TRAILER = """
+</UL>
+
+</BODY>
+
+</HTML>
+"""
+
 
 
 class hdl_access(nullAPI.null_access):
@@ -68,19 +83,6 @@ class hdl_access(nullAPI.null_access):
 	# respectively
 	self._uri = uri
 	self._types = HANDLE_TYPES
-	self._chooser = HandleResolutionChooser()
-
-    def set_chooser(self, chooser):
-	oldchooser = self._chooser
-	self._chooser = chooser
-	return oldchooser
-
-    def _error(self):
-	import sys
-	import TbDialog
-	from __main__ import app
-	TbDialog.TracebackDialog(app.root, sys.exc_type,
-				 sys.exc_value, sys.exc_traceback)
 
     def pollmeta(self):
 	nullAPI.null_access.pollmeta(self)
@@ -92,16 +94,34 @@ class hdl_access(nullAPI.null_access):
 	    # catch all errors and raise an IOError.  The Grail
 	    # protocol extension defines this as the only error we're
 	    # allowed to raise.
-#	    self._error()
-	    raise IOError
+	    raise IOError, "hash table failure"
 	return 'Ready', 1
 
     def getmeta(self):
 	nullAPI.null_access.getmeta(self)
-	try:
-	    flags, uri = self._chooser.choose(self._items)
-	except:
-	    self._error()
-	    raise IOError
-	# return `document has temporarily moved'
-	return 302, 'Moved', {'location': uri}
+	if len(self._items) == 1:
+	    flags, uri = self._items[0]
+	    return 302, 'Moved', {'location': uri}
+	data = HTML_HEADER
+	for flags, uri in self._items:
+	    text = '<LI><A HREF="%s">%s</A>\n' % (uri, uri)
+	    data = data + text
+	data = data + HTML_TRAILER
+	self._data = data
+	self._pos = 0
+	return 200, 'OK', {'content-type': 'text/html'}
+
+    # polldata() is inherited from nullAPI
+
+    def getdata(self, maxbytes):
+	end = self._pos + maxbytes
+	data = self._data[self._pos:end]
+	if not data:
+	    return nullAPI.null_access.getdata(self, maxbytes)
+	self._pos = end
+	return data
+
+
+# Here are some handles to try out the multiple-URL reponse:
+# hdl://nonreg.guido/python-home-page
+# hdl://nonreg.guido/python-ftp-dir
