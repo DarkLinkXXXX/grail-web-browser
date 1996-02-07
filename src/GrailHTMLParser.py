1a62cc3c2471f2e4f0a7f20f9559ad7c9f4c7b01
guido <guido>
1996-02-07 19:37:56 +0000

    Support for Java style <APPLET> tag added.  Moved all applet loading mechanics to AppletLoader.py.  Changed <APP> tag handling to also use this.  This gives width and height their Java meaning on the <APP> tag (previously, they were passed to the applet; now they are used to set the initial size of the frame widget).

--- src/GrailHTMLParser.py
+++ src/GrailHTMLParser.py
@@ -8,19 +8,20 @@
 from Tkinter import *
 import htmllib
 import urlparse
-import urllib
 import string
 import tktools
 import formatter
-from BaseReader import BaseReader
+from AppletLoader import AppletLoader
 
 
-# Get rid of do_isindex method to we can implement it as an extension
+# Get rid of do_isindex method so we can implement it as an extension
 if hasattr(htmllib.HTMLParser, 'do_isindex'):
     del htmllib.HTMLParser.do_isindex
 
 
 class AppletHTMLParser(htmllib.HTMLParser):
+
+    insert_aware_tags = ['param', 'alias', 'insert', 'applet', 'embed']
 
     def __init__(self, viewer, reload=0):
 	self.viewer = viewer
@@ -32,11 +33,29 @@ class AppletHTMLParser(htmllib.HTMLParser):
 	self.style_stack = []
 	self.loaded = []
 	self.page_tag_count = 0
+	self.insert_stack = []
+	self.insert_active = 0		# Length of insert_stack at activation
 
     def close(self):
 	htmllib.HTMLParser.close(self)
 
     # Override HTMLParser internal methods
+
+    def handle_starttag(self, tag, method, attrs):
+	if self.insert_active:
+	    if tag not in self.insert_aware_tags:
+		return
+	method(attrs)
+
+    def handle_endtag(self, tag, method):
+	if self.insert_active:
+	    if tag not in self.insert_aware_tags:
+		return
+	method()
+
+    def handle_data(self, data):
+	if not self.insert_active:
+	    htmllib.HTMLParser.handle_data(self, data)
 
     def anchor_bgn(self, href, name, type):
 	self.formatter.flush_softspace()
@@ -119,94 +138,90 @@ class AppletHTMLParser(htmllib.HTMLParser):
     def end_center(self):
 	self.formatter.pop_style()
 
+    # New tag: <INSERT> (for the time being, same as <APPLET>)
+
+    def start_insert(self, attrs):
+	self.start_applet(attrs)
+
+    def end_insert(self):
+	self.end_applet()
+
+    # New tag: <APPLET>
+
+    def start_applet(self, attrs):
+	self.insert_stack.append('applet')
+	if self.insert_active: return
+	keywords = self.get_keywords(attrs)
+	# See http://www.javasoft.com/people/avh/applet.html for DTD
+	width = self.extract_keyword('width', keywords, conv=string.atoi)
+	height = self.extract_keyword('height', keywords, conv=string.atoi)
+	menu = self.extract_keyword('menu', keywords)
+	code = self.extract_keyword('code', keywords)
+	name = self.extract_keyword('name', keywords)
+	codebase = self.extract_keyword('codebase', keywords)
+	align = self.extract_keyword('align', keywords, 'baseline')
+	vspace = self.extract_keyword('vspace', keywords)
+	hspace = self.extract_keyword('hspace', keywords)
+	apploader = AppletLoader(self,
+				 width=width, height=height, menu=menu,
+				 name=name, code=code, codebase=codebase,
+				 vspace=vspace, hspace=hspace, align=align,
+				 reload=self.reload)
+	if apploader.feasible():
+	    self.apploader = apploader
+	    self.insert_active = len(self.insert_stack)
+	else:
+	    apploader.close()
+
+    def end_applet(self):
+	if self.insert_active == len(self.insert_stack):
+	    self.insert_active = 0
+	    self.apploader.go_for_it()
+	del self.insert_stack[-1]
+
+    # New tag: <PARAM>
+
+    def do_param(self, attrs):
+	if 0 < self.insert_active == len(self.insert_stack):
+	    name = value = None
+	    for a, v in attrs:
+		if a == 'name': name = v
+		if a == 'value': value = v
+	    if name is not None and value is not None:
+		self.apploader.set_param(name, value)
+
     # New tag: <APP>
 
     def do_app(self, attrs):
 	keywords = self.get_keywords(attrs)
 	mod, cls, src = self.get_mod_class_src(keywords)
-	if keywords.has_key('menu'):
-	    menuname = keywords['menu']
-	    del keywords['menu']
-	    browser = self.browser
-	    menubutton = Menubutton(browser.mbar, text=menuname)
-	    menubutton.pack(side=LEFT)
-	    menu = AppletMenu(menubutton, self)
-	    menubutton['menu'] = menu
-	    browser.user_menus.append(menubutton)
-	    parent = menu
+	if not (mod and cls): return
+	width = self.extract_keyword('width', keywords, conv=string.atoi)
+	height = self.extract_keyword('height', keywords, conv=string.atoi)
+	menu = self.extract_keyword('menu', keywords)
+	mod = mod + ".py"
+	apploader = AppletLoader(self, code=mod, name=cls, codebase=src,
+				 width=width, height=height, menu=menu,
+				 reload=self.reload)
+	if apploader.feasible():
+	    for name, value in keywords.items():
+		apploader.set_param(name, value)
+	    apploader.go_for_it()
 	else:
-	    bg = self.viewer.text['background']
-	    frame = AppletFrame(self.viewer.text, self, background=bg)
-	    self.add_subwindow(frame)
-	    parent = frame
+	    apploader.close()
 
-	try:
-	    m = None
-	    if not self.reload:
-		m = self.mod_is_loaded(mod)
-		if not m:
-		    stuff = self.mod_is_local(mod)
-		    if stuff:
-			m = self.load_module(mod, stuff)
-	    else:
-		stuff = self.mod_is_local(mod)
-		if stuff:
-		    if mod in self.loaded:
-			file = stuff[0]
-			if file and hasattr(file, 'close'):
-			    file.close()
-			m = self.mod_is_loaded(mod)
-		    else:
-			self.loaded.append(mod)
-			m = self.load_module(mod, stuff)
-	    if m:
-		if not hasattr(m, cls):
-		    raise NameError, \
-			  "module %s has no class %s" % (mod, cls)
-		C = getattr(m, cls)
-		apply(C, (parent,), keywords)
-		return
-
-	    # Start loading the module source now
-	    dirurl = urlparse.urljoin(self.browser.baseurl(), src or '.')
-	    modcomps = string.splitfields(mod, '.')
-	    modfile = string.joinfields(modcomps, '/')
-	    modurl = urlparse.urljoin(dirurl, modfile + ".py")
-	    api = self.app.open_url(modurl, 'GET', {}, self.reload)
-	    whatnext = (parent, mod, cls, src, keywords, self)
-	    reader = ModuleReader(self.browser, api, whatnext)
-	except:
-	    self.show_tb()
-	    return
-
-
-    # Subroutines for applet creation
+    # Subroutines for <APP> tag parsing
 
     def get_keywords(self, attrs):
 	keywords = {}
 	for a, v in attrs:
-	    try: v = string.atoi(v, 0)
-	    except string.atoi_error:
-		try: v = string.atol(v, 0)
-		except string.atol_error:
-		    try: v = string.atof(v)
-		    except string.atof_error: pass
 	    keywords[a] = v
 	return keywords
 
     def get_mod_class_src(self, keywords):
-	cls = None
-	src = ''
-	if keywords.has_key('class'):
-	    cls = keywords['class']
-	    del keywords['class']
-	if keywords.has_key('src'):
-	    src = keywords['src']
-	    del keywords['src']
-	if not cls:
-	    print "*** APP tag has no CLASS attribute"
-	    return None, None, None
-	if '.' in cls:
+	cls = self.extract_keyword('class', keywords)
+	src = self.extract_keyword('src', keywords)
+	if cls and '.' in cls:
 	    i = string.rfind(cls, '.')
 	    mod = cls[:i]
 	    cls = cls[i+1:]
@@ -214,43 +229,18 @@ class AppletHTMLParser(htmllib.HTMLParser):
 	    mod = cls
 	return mod, cls, src
 
-    def get_class_proper(self, mod, cls, src):
-	rexec = self.browser.app.rexec
-	rexec.reset_urlpath()
-	url = urlparse.urljoin(self.browser.baseurl(), src or '.')
-	rexec.set_urlpath(url)
-	if self.reload and rexec.modules.has_key(mod) and \
-	   mod not in self.loaded:
-	    # XXX Hack, hack
-	    msg, crs = self.viewer.browser.message("Reloading module " + mod)
-	    m = rexec.modules[mod]
-	    rexec.r_reload(m)
+    def extract_keyword(self, key, keywords, default=None, conv=None):
+	if keywords.has_key(key):
+	    value = keywords[key]
+	    del keywords[key]
+	    if not conv:
+		return value
+	    try:
+		return conv(value)
+	    except:
+		return default
 	else:
-	    msg, crs = self.viewer.browser.message("Loading module " + mod)
-	    m = rexec.r_import(mod)
-	self.loaded.append(mod)
-	self.viewer.browser.message(msg, crs)
-	return getattr(m, cls)
-
-    def mod_is_loaded(self, mod):
-	rexec = self.browser.app.rexec
-	try:
-	    return rexec.modules[mod]
-	except KeyError:
-	    return None
-
-    def mod_is_local(self, mod):
-	rexec = self.browser.app.rexec
-	path = rexec.get_url_free_path()
-	return rexec.loader.find_module(mod, path)
-
-    def load_module(self, mod, stuff):
-	rexec = self.browser.app.rexec
-	rexec.reset_urlpath()
-	return rexec.loader.load_module(mod, stuff)
-
-    def show_tb(self):
-	self.app.exception_dialog("during applet loading")
+	    return default
 
     # Handle HTML extensions
 
@@ -265,60 +255,3 @@ class AppletHTMLParser(htmllib.HTMLParser):
 	function = app.find_html_end_extension(tag)
 	if function:
 	    function(self)
-
-
-class ModuleReader(BaseReader):
-
-    """Load an applet, asynchronously.
-
-    First load an applet's source module into the cache.  Once it's
-    done, invoke the standard mechanism to actually load the module.
-    This will find the source ready for it in the cache.
-
-    """
-
-    def __init__(self, browser, api, whatnext):
-	self.whatnext = whatnext
-	BaseReader.__init__(self, browser, api)
-
-    def handle_data(self, data):
-	pass
-
-    def handle_error(self, errno, errmsg, headers):
-	BaseReader.handle_error(self, errno, errmsg, headers)
-
-    def handle_eof(self):
-	self.stop()
-	parent, mod, cls, src, keywords, parser = self.whatnext
-	try:
-	    C = parser.get_class_proper(mod, cls, src)
-	    apply(C, (parent,), keywords)
-	except:
-	    parser.show_tb()
-
-
-class AppletMagic:
-
-    def __init__(self, parser):
-	self.grail_parser = parser
-	self.grail_viewer = self.grail_browser = self.grail_app = None
-	if parser:
-	    self.grail_viewer = viewer = parser.viewer
-	    if viewer:
-		self.grail_browser = browser = viewer.browser
-		if browser:
-		    self.grail_app = app = browser.app
-
-
-class AppletFrame(Frame, AppletMagic):
-
-    def __init__(self, master, parser=None, cnf={}, **kw):
-	apply(Frame.__init__, (self, master, cnf), kw)
-	AppletMagic.__init__(self, parser)
-
-
-class AppletMenu(Menu, AppletMagic):
-
-    def __init__(self, master, parser=None, cnf={}, **kw):
-	apply(Menu.__init__, (self, master, cnf), kw)
-	AppletMagic.__init__(self, parser)
