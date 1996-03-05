129b7a34597e4654ce18ebfc938e1d01a29c63ff
guido <guido>
1996-03-05 23:22:54 +0000

    Support for directory listings. Note: when listing a directory, it does *not* default to index.html, since otherwise you wouldn't be able to see other files in a directory...

--- src/protocols/fileAPI.py
+++ src/protocols/fileAPI.py
@@ -1,16 +1,42 @@
 from assert import assert
+import os
 
 META, DATA, DONE = 'META', 'DATA', 'DONE'
+
+LISTING_HEADER = """<HTML>
+<HEAD>
+<BASE HREF="%(url)s">
+<TITLE>Local Directory: %(url)s</TITLE>
+</HEAD>
+<BODY>
+<H1>Local Directory: %(pathname)s</H1>
+<PRE>"""
+
+LISTING_TRAILER = """</PRE>
+</BODY>
+"""
+
+LISTING_PATTERN = """\
+^\([-a-z][-a-z][-a-z][-a-z][-a-z][-a-z][-a-z][-a-z][-a-z][-a-z]\)\
+\([ \t]+.*[ \t]+\)\([^ \t]+\)$"""
 
 class file_access:
 
     def __init__(self, url, method, params):
-	self.fp = open(url)		# May raise IOError!
-	from __main__ import app
-	ctype, cencoding = app.guess_type(url)
+	from urllib import unquote
+	self.url = url
+	self.pathname = unquote(self.url)
+	self.method = method
+	self.params = params
 	self.headers = {}
-	if ctype: self.headers['content-type'] = ctype
-	if cencoding: self.headers['content-encoding'] = cencoding
+	if os.path.isdir(self.url):
+	    self.format_directory()
+	else:
+	    self.fp = open(self.pathname) # May raise IOError!
+	    from __main__ import app
+	    ctype, cencoding = app.guess_type(self.pathname)
+	    if ctype: self.headers['content-type'] = ctype
+	    if cencoding: self.headers['content-encoding'] = cencoding
 	self.state = META
 
     def pollmeta(self):
@@ -34,10 +60,60 @@ class file_access:
 	return data
 
     def fileno(self):
-	return self.fp.fileno()
+	try:
+	    return self.fp.fileno()
+	except AttributeError:
+	    return -1
 
     def close(self):
 	fp = self.fp
 	self.fp = None
 	if fp:
 	    fp.close()
+
+    def format_directory(self):
+	# XXX Unixism
+	if self.url and self.url[-1] != '/':
+	    self.url = self.url + '/'
+	fp = os.popen("ls -l -a %s 2>&1" % self.pathname, "r")
+	lines = fp.readlines()
+	fp.close()
+	import StringIO
+	import regex
+	from urllib import quote
+	from urlparse import urljoin
+	import regsub
+	def escape(s, regsub=regsub):
+	    if not s: return ""
+	    s = regsub.gsub('&', '&amp;', s) # Must be done first
+	    s = regsub.gsub('<', '&lt;', s)
+	    s = regsub.gsub('>', '&gt;', s)
+	    return s
+	prog = regex.compile(self.listing_pattern)
+	data = self.listing_header % {'url': self.url,
+				      'pathname': escape(self.pathname)}
+	for line in lines:
+	    if line[-1] == '\n': line = line[:-1]
+	    if prog.match(line) < 0:
+		line = escape(line) + '\n'
+		data = data + line
+		continue
+	    mode, middle, name = prog.group(1, 2, 3)
+	    rawname = name
+	    [mode, middle, name] = map(escape, [mode, middle, name])
+	    href = urljoin(self.url, quote(rawname))
+	    if len(mode) == 10 and mode[0] == 'd' or name[-1:] == '/':
+		if name[-1:] != '/':
+		    name = name + '/'
+		if href[-1:] != '/':
+		    href = href + '/'
+	    line = '%s%s<A HREF="%s">%s</A>\n' % (
+		mode, middle, escape(href), name)
+	    data = data + line
+	data = data + self.listing_trailer
+	self.fp = StringIO.StringIO(data)
+	self.headers['content-type'] = 'text/html'
+
+    listing_header = LISTING_HEADER
+    listing_trailer = LISTING_TRAILER
+    listing_pattern = LISTING_PATTERN
