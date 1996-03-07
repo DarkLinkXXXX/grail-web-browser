b68ae550d397b053b30340f24d85b1f64466c053
bwarsaw <bwarsaw>
1996-03-07 21:55:04 +0000

    Integrated with Grail, disabled by default.  Provides lots of (hopefully) useful docstrings and some higher level functionality.

--- src/ancillary/RemoteControl.py
+++ src/ancillary/RemoteControl.py
@@ -1,21 +1,156 @@
-# This class provides the structure for a generalized remote control
-# operation of Grail.  You can instantiate and populate this class in
-# your $GRAILDIR/user/grailrc.py file.  By doing so, you can allow
-# remote control of any of Grail's functionality.
-#
-# On security: there is no real security model here.  You need to use
-# your own head about this.  This class operates in unprotected mode,
-# so be very careful what you open up.  Unix domain sockets are used
-# so only the Grail user on the local host should be able to send
-# commands to this interface.  Still how secure is that?
-#
-# This module essentially opens the socket and registers it with Tk so
-# when data is readable on it, registered callbacks are executed.
-# This is a really simple minded string based protocol, with a
-# synchronous server model.  It's also only one-way.  Right now, it
-# doesn't send any data back to the client.  Commands are limited to
-# 1024 bytes.
+"""Remote control of Grail.
 
+By default, remote control of Grail is stopped.  You can turn remote
+control on by using start() function in this module.  Likewise, you
+can turn it off again by using the stop() function.  You would do this
+from your Grail initialization file, typically
+$GRAILDIR/user/grailrc.py.
+
+N.B. Only one Grail can be remoted controlled at a time, so if you
+think you might be starting up more than one Grail process, your
+grailrc.py file should catch ClashError, which should just bypass any
+remote control registrations you do.
+
+Remote control is based on a simple string command protocol.  When the
+remote controller sees a string show up on it's socket, it parses for
+the first space delimited word, which provides the name of the
+command.  You must register handlers for all valid command strings.
+If Grail encounters a command string it doesn't know about, it
+displays an error dialog.
+
+To register new command string handlers, use the register() function.
+Likewise there is an unregister function().  Two convenience routines
+are provided for you, register_loads() and unregister_loads().
+
+On security: there is no real security model here.  You need to use
+your own head about this.  This class operates in unprotected mode,
+so be very careful what you open up.  Unix domain sockets are used
+so only the Grail user on the local host should be able to send
+commands to this interface.  Still how secure is that?
+
+On Unix systems with the DISPLAY environment variable set, the socket
+file name will typically be /tmp/.grail-unix/$USER-$DISPLAY, and the
+directory will be protected with mode 0700.  The actual file name used
+is available from the rendezvous_name() function.  You can also define
+what file to use by setting the environment variable GRAIL_REMOTE.
+TBD: this should also be made a preference.
+
+This module essentially opens the socket and registers it with Tk so
+when data is readable on it, registered callbacks are executed.  This
+is a really simple minded string based protocol, with a synchronous
+server model.  It's also only one-way.  Right now, it doesn't send any
+data back to the client.  Commands are limited to 1024 bytes.
+
+TBD: Port this to non-Unix systems.
+
+Exported functions:
+
+start()
+	starts the remote controller
+
+stop()
+	stops the remote controller
+
+register(cmdstr, callback)
+	registers a callback function for handling cmdstr commands.
+	The callback has the following form:
+
+	callback(cmdstr, cmdargs)
+
+	where cmdargs is the remainder of the string read from the
+	socket.  Note that you can register more than one callback for
+	a particular command string; they are invoked in the order
+	they were registered.
+
+unregister(cmdstr, callback=None)
+	unregisters the callback for the given command string.  If
+	callback is not specified, all callbacks for the cmdstr are
+	unregistered.
+
+register_loads()
+	registers a callback for the command string 'LOAD' which loads
+	a given URL into the latest browser window.  The URL to load
+	is the cmdargs for the command.  This also registers the
+	'LOADNEW' command string which pops up a new browser and loads
+	the URL into that browser window.
+
+unregister_loads()
+	unregisters the built-in LOAD and LOADNEW callbacks.
+
+rendezvous_name()
+	returns the absolute path to the socket file.
+
+
+Exported exceptions:
+
+InitError       - the socket couldn't be initialized
+ClashError      - another Grail is already being remote controlled
+BadCommandError - a badly formatted command was received
+NoHandlerError  - no handler for the received command has been registered
+
+
+Example (put this in your grailrc.py file):
+
+# Turn on remote control.  Ignore error that get's raised if some
+# other Grail is being remote controlled.
+import RemoteControl
+RemoteControl.register_loads()
+try:
+    RemoteControl.start()
+except RemoteControl.ClashError:
+    pass
+
+"""
+
+
+# errors
+InitError = 'RemoteControl.InitError'
+ClashError = 'RemoteControl.ClashError'
+BadCommandError = 'RemoteControl.BadCommandError'
+NoHandlerError = 'RemoteControl.NoHandlerError'
+
+_controller = None
+_filename = None
+_loads_registered = None
+
+def _create():
+    global _controller
+    if not _controller:
+	_controller = Controller()
+
+def start():
+    _create()
+    _controller.start()
+
+def stop():
+    if _controller:
+	_controller.stop()
+
+def register(cmdstr, callback):
+    _create()
+    _controller.register(cmdstr, callback)
+
+def unregister(cmdstr, callback):
+    _create()
+    _controller.unregister(cmdstr, callback)
+
+def register_loads():
+    _create()
+    global _loads_registered
+    if not _loads_registered:
+	_controller.register('LOAD', _controller.load_cmd)
+	_controller.register('LOADNEW', _controller.load_new_cmd)
+	_loads_registered = 1
+
+def unregister_loads():
+    global _loads_registered
+    if _loads_registered:
+	_controller.unregister('LOAD', _controller.load_cmd)
+	_controller.unregister('LOADNEW', _controller.load_new_cmd)
+	_loads_registered = None
+
+
+
 import tempfile
 import os
 import socket
@@ -25,23 +160,18 @@ import tkinter
 from grailutil import *
 
 # The file structure.  Modeled after X11
-TMPDIR = tempfile.gettempdir()
-USER = getenv('USER') or getenv('LOGNAME')
-XDISPLAY = getenv('DISPLAY') or ':0'
-RC_FILENAME = os.path.join(TMPDIR,
-			   os.path.join('.grail-unix',
-					'%s-%s' % (USER, XDISPLAY)))
-
-# errors
-RC_SOCKET_EXISTS = 'RC_SOCKET_EXISTS'
-RC_NO_FILENO = 'RC_NO_FILENO'
-RC_BAD_COMMAND_FORMAT = 'RC_BAD_COMMAND_FORMAT'
-RC_NO_COMMAND = 'RC_NO_COMMAND'
-
+_filename = getenv('GRAIL_REMOTE')
+if not _filename:
+    TMPDIR = tempfile.gettempdir()
+    USER = getenv('USER') or getenv('LOGNAME')
+    XDISPLAY = getenv('DISPLAY') or ':0'
+    _filename = os.path.join(TMPDIR,
+			     os.path.join('.grail-unix',
+					  '%s-%s' % (USER, XDISPLAY)))
 
 
-class RemoteControl:
-    def __init__(self, path=RC_FILENAME):
+class Controller:
+    def __init__(self, path=_filename):
 	# register a destruction handler with the Grail Application object
 	from __main__ import app
 	self._app = app
@@ -50,44 +180,51 @@ class RemoteControl:
 	self._path = path
 	self._fileno = None
 	self._socket = None
+	self._enabled = None
 	# Don't create the socket now, because we want to allow
 	# clients of this class to register callbacks for commands
 	# first.
 	self._cbdict = {}
 	self._cmdre = regex.compile('\([^ \t]+\)\(.*\)')
 
-    def begin(self):
-	"""Begin listening for remote control commands.
-
-	You should have registered all your callbacks before calling
-	this method, but that's not required.
-	"""
-	# for security, create the file structure
-	head, self._filename = os.path.split(self._path)
-	dirhier = []
-	while head and not os.path.isdir(head):
-	    head, t = os.path.split(head)
-	    dirhier.insert(0, t)
-	for dir in dirhier:
-	    head = os.path.join(head, dir)
-	    os.mkdir(head, 0700)
-	self._filename = self._path
-	# What do we do with multiple Grail processes?  Which one do
-	# we remote control?  That's a higher level decision.
-	if os.path.exists(self._filename):
-	    raise RC_SOCKET_EXISTS, \
-		  'Grail remote control file already in use: ' + self._filename
-	# create the FIFO object
-	s = self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	s.bind(self._filename)
-	s.listen(1)
-	# register with Tk
-	self._fileno = s.fileno()
-	if self._fileno >= 0:
+    def start(self):
+	"""Begin listening for remote control commands."""
+	# initialize the socket
+	if self._fileno is None:
+	    # for security, create the file structure
+	    head, self._filename = os.path.split(self._path)
+	    dirhier = []
+	    while head and not os.path.isdir(head):
+		head, t = os.path.split(head)
+		dirhier.insert(0, t)
+	    for dir in dirhier:
+		head = os.path.join(head, dir)
+		os.mkdir(head, 0700)
+	    self._filename = self._path
+	    # TBD: What do we do with multiple Grail processes?  Which
+	    # one do we remote control?
+	    if os.path.exists(self._filename):
+		raise ClashError
+	    # create the FIFO object
+	    s = self._socket = socket.socket(socket.AF_UNIX,
+					     socket.SOCK_STREAM)
+	    s.bind(self._filename)
+	    s.listen(1)
+	    # register with Tk
+	    self._fileno = s.fileno()
+	    if self._fileno < 0:
+		self._fileno = None
+		raise InitError
+	if not self._enabled:
+	    self._enabled = 1
 	    tkinter.createfilehandler(
 		self._fileno, tkinter.READABLE, self._dispatch)
-	else:
-	    raise RC_NO_FILENO, 'Could not get fileno for Tk'
+
+    def stop(self):
+	"""Stop listening for remote control commands."""
+	if self._enabled:
+	    self._enabled = None
+	    tkinter.deletefilehandler(self._fileno)
 
     def register(self, cmdstr, callback):
 	"""Register command string, callback function pairs.
@@ -121,11 +258,12 @@ class RemoteControl:
 	    else:
 		del self._cbdict[cmdstr]
 
-    ## private methods
+    # private methods
 
     def _close(self):
-	if self._fileno: tkinter.deletefilehandler(self._fileno)
-	if self._filename: os.unlink(self._filename)
+	self.stop()
+	if self._filename:
+	    os.unlink(self._filename)
 
     def _dispatch(self, *args):
 	conn, addr = self._socket.accept()
@@ -133,13 +271,36 @@ class RemoteControl:
 	# strip off the command string
 	string.strip(rawdata)
 	if self._cmdre.match(rawdata) < 0:
-	    raise RC_BAD_COMMAND_FORMAT, 'Bad remote command: ' + rawdata
+	    raise BadCommandError, 'Bad remote command: ' + rawdata
 	# extract the command and args strings
 	command = string.strip(self._cmdre.group(1))
 	argstr = string.strip(self._cmdre.group(2))
 	# look up the command string
 	if not self._cbdict.has_key(command):
-	    raise RC_NO_COMMAND, 'No callbacks for command: ' + command
+	    raise NoHandlerError, 'No callbacks for command: ' + command
 	cblist = self._cbdict[command]
 	# call all callbacks in list
-	for cb in cblist: cb(command, argstr)
+	for cb in cblist:
+	    cb(command, argstr)
+
+    # convenience methods
+
+    def _do_load(self, uri, in_new_window=None):
+	# get a reference to one of the browsers in the application
+	browsers = self._app.browsers
+	if len(browsers) > 0:
+	    b = browsers[-1]
+	else:
+	    return
+	# create a new browser toplevel?
+	if in_new_window:
+	    import Browser
+	    b = Browser.Browser(b.master, self._app)
+	# tell it to load the URL
+	b.load(uri)
+
+    def load_cmd(self, cmdstr, argstr):
+	self._do_load(argstr)
+
+    def load_new_cmd(self, cmdstr, argstr):
+	self._do_load(argstr, in_new_window=1)
