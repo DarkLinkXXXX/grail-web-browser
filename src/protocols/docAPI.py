ed566a63dc7f65675252d5640b2daa2820539922
guido <guido>
1996-03-25 18:42:24 +0000

    "doc:" URLs now reference the grail-0.3 directory instead of 0.2. They use error code 301 (permanently redirected) instead of 302 (temporarily redirected).

--- src/protocols/docAPI.py
+++ src/protocols/docAPI.py
@@ -7,8 +7,8 @@ class doc_access(null_access):
     def __init__(self, url, method, params):
 	null_access.__init__(self, url, method, params)
 	if url[:1] != '/': url = '/' + url
-	self.url = "http://monty.cnri.reston.va.us/grail-0.2" + url
+	self.url = "http://monty.cnri.reston.va.us/grail-0.3" + url
 
     def getmeta(self):
 	null_access.getmeta(self)	# assert, state change
-	return 302, "Moved", {'location': self.url}
+	return 301, "Redirected", {'location': self.url}
