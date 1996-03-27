c1eda766ccb30ee2ce23834d0ae8ba2f6168e80e
bwarsaw <bwarsaw>
1996-03-27 18:37:43 +0000

    Checkpointing... simple tables look pretty good and the fireworks are kept to a minimum.  Cells that span columns work correctly, but not row spanning cells.  Captions aren't aligned correctly yet, either.
    I'm going to live with the fireworks for now so I can finish up the
    functionality.  Once that's done, and there's time, I'll try to clean
    up the fireworks even more, if possible.

--- src/html/table.py
+++ src/html/table.py
@@ -2,7 +2,7 @@
 
 """
 # $Source: /home/john/Code/grail/src/html/table.py,v $
-__version__ = '$Id: table.py,v 2.3 1996/03/23 00:41:15 bwarsaw Exp $'
+__version__ = '$Id: table.py,v 2.4 1996/03/27 18:37:43 bwarsaw Exp $'
 
 
 import string
@@ -136,6 +136,49 @@ class TableSubParser:
 
 
 
+def _get_widths(tw):
+    width_max = 0
+    border_x = None
+    # get maximum width of cell: the longest line with no line wrapping
+    tw['wrap'] = NONE
+    border_x, y, w, h, b = tw.dlineinfo(1.0)
+    linecnt = string.atoi(string.splitfields(tw.index(END), '.')[0])
+    for lineno in range(1, linecnt):
+	index = '%d.0' % lineno
+	tw.see(index)
+	x, y, w, h, b = tw.dlineinfo(index)
+	width_max = max(width_max, w)
+    width_max = width_max + (2 * border_x)
+    # get minimum width of cell: longest word
+    tw['wrap'] = WORD
+    contents = tw.get(1.0, END)
+    longest_word = reduce(max, map(len, string.split(contents)), 0) + 1
+    tw['width'] = longest_word
+    width_min = tw.winfo_reqwidth() #+ (2 * border_x)
+    return width_min, width_max
+
+def _get_height(tw):
+    linecount = string.atoi(string.splitfields(tw.index(END), '.')[0])
+    tw['height'] = linecount
+    tw.update_idletasks()
+    tw.see(1.0)
+    x, border_y, w, h, b = tw.dlineinfo(1.0)
+    loopcnt = 0
+    while 1:
+	loopcnt = loopcnt + 1
+	if loopcnt > 100:
+	    raise 'Loop Badness Detected!'
+	info = tw.dlineinfo('end - 1 c')
+	if info:
+	    break
+	linecount = linecount + 1
+	tw['height'] = linecount
+	tw.update_idletasks()
+    x, y, w, h, b = info
+    return border_y + y + h
+
+
+
 class AttrElem:
     """Base attributed table element.
 
@@ -159,37 +202,6 @@ class AttrElem:
 
 
 
-def _get_real_size(textwidget):
-    textwidget.see(1.0)
-    # TBD: this sucks, since the user will see all the gross resizing
-    # going on.  I really wish there was a way to hide this from
-    # happening, but without the update_idletasks(), dlineinfo() won't
-    # work.
-    textwidget.update_idletasks()
-    x, y, w, h, baseline = textwidget.dlineinfo('end - 1 c')
-    return 2*x + w, 2*y + h
-
-def _get_longest_line(textwidget, wrap=NONE):
-    textwidget['wrap'] = wrap
-    # TBD: another gross hack.  See above...
-    textwidget.update_idletasks()
-    longest = 0
-    linecnt = string.atoi(string.splitfields(textwidget.index(END), '.')[0])
-    for lineno in range(1, linecnt):
-	index = '%d.0' % lineno
-	textwidget.see(index)
-	x, y, w, h, baseline = textwidget.dlineinfo(index)
-	longest = max(longest, w)
-    return longest
-
-def _get_longest_word(textwidget):
-    contents = textwidget.get(1.0, END)
-    longest = reduce(max, map(len, string.split(contents)), 0)
-    textwidget['width'] = longest + 1
-    return _get_longest_line(textwidget, wrap=WORD)
-
-
-
 class Table(AttrElem):
     """Top level table information object.
 
@@ -200,17 +212,14 @@ class Table(AttrElem):
 	AttrElem.__init__(self, attrs)
 	self.parentviewer = parentviewer
 	self.parentviewer.text.insert(END, '\n')
-## 	self.parentviewer.text.propagate(0)
 	# public ivars
 	self.layout = self.attribute('cols') and FIXEDLAYOUT or AUTOLAYOUT
 	# geometry
 	self.frame = Frame(parentviewer.text,
 			   borderwidth=1,
-			   relief=RAISED,
-			   width=1000000, height=1000000)
+			   relief=RAISED)
 	self.frame.pack()
 	self.cellcontainer = Canvas(self.frame, relief=FLAT)
-	self.cellcontainer.pack()
 	self.caption = None
 	self.cols = []			## multiple COL or COLGROUP
 	self.colgroups = []
@@ -222,12 +231,13 @@ class Table(AttrElem):
 
     def finish(self):
 	if self.layout == AUTOLAYOUT:
+	    # TBD: temporary, should be add_subwindow() instead
+	    self.cellcontainer.pack()
 	    containerwidth = self._autolayout_1()
 	    if self.caption:
-		self.caption.situate(width=containerwidth)
+		self.caption.finish()
 	else:
 	    pass
-## 	self.parentviewer.text.propagate(1)
 
     def _autolayout_1(self):
 	# internal representation of the table as a sparse array
@@ -280,7 +290,6 @@ class Table(AttrElem):
 		    cs = col + 1
 		    for cs in range(cs, cs + cell.colspan - 1):
 			for rs in range(row, row + cell.rowspan):
-			    print 'occupying:', rs, cs
 			    table[(rs, cs)] = OCCUPIED
 		    col = cs
 		row = row + 1
@@ -306,7 +315,20 @@ class Table(AttrElem):
 	for col in range(colcount):
 	    for row in range(rowcount):
 		cell = table[(row, col)]
-		if cell not in [EMPTY, OCCUPIED]:
+		if cell in [EMPTY, OCCUPIED]:
+		    # empty cells don't contribute to the width of the
+		    # column and occupied cells have already
+		    # contributed to column widths
+		    continue
+		elif cell.colspan > 1:
+		    # cells that span more than one column evenly
+		    # apportion the min/max widths to each of the
+		    # consituent columns (this is how Arena does it as
+		    # per the latest Table HTML spec).
+		    cellwidth = float(cell.width()) / cell.colspan
+		    for col_i in range(col, col + cell.colspan):
+			cellwidths[col_i] = max(cellwidths[col_i], cellwidth)
+		else:
 		    cellwidth = cell.width()
 		    cellheight = cell.height()
 		    cellwidths[col] = max(cellwidths[col], cellwidth)
@@ -335,93 +357,16 @@ class Table(AttrElem):
 		    cellwidth = cellwidth + cellwidths[span]
 		for span in range(row, row + cell.rowspan):
 		    cellheight = cellheight + cellheights[span]
-		cell.situate(self.cellcontainer,
-			     x=xpos, y=ypos,
+		cell.situate(x=xpos, y=ypos,
 			     w=cellwidth, h=cellheight)
 		xpos = xpos + cellwidth
 	    ypos = ypos + tallest
 
-	self.frame.propagate(1)
 	self.parentviewer.add_subwindow(self.frame)
 	self.cellcontainer.config(width=canvaswidth, height=ypos)
 	self.parentviewer.text.insert(END, '\n')
 	return canvaswidth
 	    
-
-##     def _do_autolayout(self):
-## 	# go through each body part and figure out how many columns we
-## 	# need.
-## 	columncnt = 0
-## 	rowcnt = 0
-## 	bodies = (self.thead or []) + self.tbodies + (self.tfoot or [])
-## 	for tb in bodies:
-## 	    columncnt = max(columncnt, tb.get_column_count())
-## 	for tb in bodies:
-## 	    rowcnt = rowcnt + len(tb.trows)
-## 	# create the table structure
-## 	for row in range(rowcnt):
-## 	    self.t.append([None] * columncnt)
-## 	# gather cells into columns, while calculating mins/maxs
-## 	colmins = [0] * columncnt
-## 	colmaxs = [0] * columncnt
-## 	row_i = 0
-## 	for tb in bodies:
-## 	    for row in tb.trows:
-## 		col_i = 0
-## 		for cell in row.cells:
-## 		    # put cell in table
-## 		    self.t[row_i][col_i] = cell
-## 		    # calculate min and max
-## 		    colmins[col_i] = max(colmins[col_i], cell.min_nonaligned)
-## 		    colmaxs[col_i] = max(colmaxs[col_i], cell.maximum)
-## 		    col_i = col_i + 1
-## 		row_i = row_i + 1
-## 	parentwidth = self.parentviewer.text['width']
-## 	minwidth = 0
-## 	for w in colmins:
-## 	    minwidth = minwidth + w
-## 	maxwidth = 0
-## 	for w in colmaxs:
-## 	    maxwidth = maxwidth + w
-## 	# case 1, minimum table width is equal to or wider than the
-## 	# available space.  assign minimum widths and allow the user
-## 	# to scroll horizontally
-## 	if minwidth >= parentwidth:
-## 	    self._resize_to(colmins)
-## 	# case 2, maximum table width fits in available space.  set
-## 	# columns to max widths
-## 	elif maxwidth < parentwidth:
-## 	    self._resize_to(colmaxs)
-## 	# case 3, max width of table is great than available space,
-## 	# but min width is smaller.  spread difference out over cells.
-## 	else:
-## 	    pass
-
-## 	# now place all the cells within the table's frame
-## 	width=0
-## 	y = 0
-## 	for row in self.t:
-## 	    x = 0
-## 	    tallest = 0
-## 	    for cell in row:
-## 		if cell:
-## 		    cell.move_to(x, y)
-## 		    x = x + cell.width()
-## 		    width = max(width, x)
-## 		    tallest = max(tallest, cell.height())
-## 	    y = y + tallest
-
-## 	print self.t
-## 	self.cellframe.place(width=width, height=y)
-
-##     def _resize_to(self, colszs):
-## 	for row in self.t:
-## 	    for i in range(len(colszs)):
-## 		size = colszs[i]
-## 		if row[i]:
-## 		    row[i].resize_to(size)
-
-
 
 class Caption(AttrElem):
     """A table caption element."""
@@ -432,24 +377,27 @@ class Caption(AttrElem):
 			     scrolling=0,
 			     stylesheet=parentviewer.stylesheet,
 			     parent=parentviewer)
-	self.viewer.frame.propagate(0)
 	self.viewer.text['relief'] = FLAT
-	align = string.lower(self.attribute('align') or '')
-	if align <> 'bottom':
-	    self.viewer.frame.pack(before=table.cellcontainer,
-				   fill=NONE, expand=NO)
-	else:
-	    self.viewer.frame.pack(fill=NONE, expand=NO)
+	self._align = string.lower(self.attribute('align') or '')
+	self._packmaster = table.cellcontainer
 
-    def situate(self, width=None):
-	if width <> None:
-	    self.viewer.text.config(width=width, height=100000, wrap=WORD)
-	    self.viewer.frame.config(width=width, height=100000)
-	    self.viewer.text.tag_add('caption', 1.0, END)
-	    self.viewer.text.tag_config('caption', justify=CENTER)
-	w, h = _get_real_size(self.viewer.text)
-	self.viewer.frame.config(width=w, height=h)
-	    
+    def finish(self):
+	fw, tw = self.viewer.frame, self.viewer.text
+	print self._align
+	if self._align == 'bottom':
+	    fw.pack(before=self._packmaster, fill=BOTH, expand=YES)
+	else:
+	    fw.pack(after=self._packmaster, fill=BOTH, expand=YES)
+	# get widths and heights.  note the width must be set before
+	# the true height can be calculated.
+	wmin, wmax = _get_widths(tw)
+	fw['width'] = wmax
+	h = _get_height(tw)
+	fw['height'] = h
+	# set the style of the contained text
+	self.viewer.text.tag_add('contents', 1.0, END)
+	self.viewer.text.tag_config('contents', justify=CENTER)
+	
 
 class Colgroup(AttrElem):
     """A column group."""
@@ -481,7 +429,9 @@ class Cell(AttrElem):
     def __init__(self, table, parser, attrs):
 	AttrElem.__init__(self, attrs)
 	self._parser = parser
-	self.viewer = Viewer(master=table.frame,
+	self._container = table.cellcontainer
+	self._cellpadding = table.attribute('cellpadding')
+	self.viewer = Viewer(master=table.cellcontainer,
 			     scrolling=0,
 			     stylesheet=parser.viewer.stylesheet,
 			     parent=parser.viewer)
@@ -493,7 +443,7 @@ class Cell(AttrElem):
 	self.min_left = 0
 	self.min_right = 0
 	self._width = 0
-	self._height = 0
+	self._height = None
 	# dig out useful attributes
 	self.rowspan = string.atoi(self.attribute('rowspan') or '1')
 	self.colspan = string.atoi(self.attribute('colspan') or '1')
@@ -507,33 +457,38 @@ class Cell(AttrElem):
 	return '"%s"' % self.viewer.text.get(1.0, END)[:-1]
 
     def width(self): return self._width
-    def height(self): return self._height
+    def height(self):
+	if self._height is None:
+	    self._height = _get_height(self.viewer.text)
+	return self._height
+
     def is_empty(self): return not self.viewer.text.get(1.0, 'end - 1 c')
 
     def finish(self):
 	if self.layout == AUTOLAYOUT:
-	    # get the real true size of the widget
-	    self._width, self._height = _get_real_size(self.viewer.text)
-	    # find the maximum width of the cell, defined to be the
-	    # width of the widest non-wrapped line
-	    self.maximum = _get_longest_line(self.viewer.text)
-	    # Now find the minimum width of the cell, defined to be
-	    # the widest word or image, taking into account leading
-	    # indents and list bullets.
-	    #
-	    # TBD: leading indents and list bullets aren't currently
-	    # counted.  This method isn't perfect, but it should be
-	    # fast and hopefully, accurate enough.
-	    self.min_nonaligned = _get_longest_word(self.viewer.text)
+	    tw = self.viewer.text
+	    # set the cellpadding
+	    padding = string.atoi(self._cellpadding or '')
+	    tw['padx'] = padding
+	    # get the cell's widths
+	    self.min_nonaligned, self._width = _get_widths(tw)
+	    self.viewer.frame['width'] = self._width
 	    # now take into account all embedded subwindows
 	    for sw in self.viewer.subwindows:
 		self.min_nonaligned = max(self.min_nonaligned, sw['width'])
+	    # Make a first pass attempt at getting the cell to the
+	    # proper size.  This isn't perfect, but it's the best we
+	    # can do without forcing an update_idletasks() and causing
+	    # fireworks.  Later, situate() will be called to do the
+	    # final sizing and positioning of the cell.
+	    tw['height'] = tw.index('end - 1 c')
 
-    def situate(self, container, x, y, w, h):
-	container.create_window(x, y,
-				window=self.viewer.frame,
-				anchor=NW,
-				width=w, height=h)
+    def situate(self, x, y, w, h):
+	self.viewer.frame.config(width=w, height=h)
+	self._container.create_window(x, y,
+				      window=self.viewer.frame,
+				      anchor=NW,
+				      width=w, height=h)
 	self._width = w
 	self._height = h
 	self._x = x
@@ -549,8 +504,8 @@ class THCell(Cell):
 
     def finish(self):
 	Cell.finish(self)
-	self.viewer.text.tag_add('centered', 1.0, END)
-	self.viewer.text.tag_config('centered', justify=CENTER)
+	self.viewer.text.tag_add('contents', 1.0, END)
+	self.viewer.text.tag_config('contents', justify=CENTER)
 
 
 if __name__ == '__main__':
