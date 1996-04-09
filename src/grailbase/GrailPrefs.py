88c29d23f18799dee0f606e638cc3ca3dbd6883a
klm <klm>
1996-04-09 00:15:19 +0000

    Details moved from module docstring to htdocs documentation.

--- src/grailbase/GrailPrefs.py
+++ src/grailbase/GrailPrefs.py
@@ -1,52 +1,11 @@
 """Functional interface to Grail user preferences.
 
-There are two preferences files.  The system-wide defaults file is located
-in the grail root directory, and named "grail-defaults".  The user's custom
-preferences is in the directory named by grailutil.getgraildir().  User
-preference settings supercede the system defaults.
-
-Interrogation for preferences which have no set value provokes a KeyError
-exception.
-
-	Preferences file format:
-
-Preferences files contain string name/value pairs, delimited like rfc822
-header fields.  Most lines start with non-whitespace alphanumerics - the
-header-field name - followed immediately by a ':'.  Typically, whitespace
-follows and then the value for that field.
-
-There are also continuation lines, which begin with whitespace, but also
-contain non-whitespace.  These lines continue the value for the most recent
-line with a header-field name.
-
-Preference key names are derived by dividing header-field names into two
-parts, a group name and a component name.  The two parts are represented
-in the header-field name delimited by '--' a pair of dashes.  Preference
-key names are case insensitive.
-
-The first line in a preferences file containing only white-space terminates
-the preferences, and a warning is emitted to stderr if there are any
-subsequent non-blank lines.
-
-Comments can be included in a preferences file by using header-style lines
-where the header-field name does *not* contain a '--' pair of dashes.
-
-Here is an example of a small preferences file:
-
-C: Comment lines here begin with "C:".
-landmarks--grail-home-page:	http://monty.cnri.reston.va.us/grail-0.2/
-C: Pref ('landmarks', 'home-page') with empty value:
-landmarks--home-page:
-C: Pref with value on continuation line (this comment cannot be between!):
-presentation--message-font:
-	-*-helvetica-medium-r-normal-*-*-100-100-*-*-*-*-*
-browser--default-height:	40
-"""
+See the Grail htdocs/info/extending/preferences.html for documentation."""
 
 # Todo:
 #  - Preference-change callback funcs
 
-__version__ = "$Revision: 2.11 $"
+__version__ = "$Revision: 2.12 $"
 # $Source: /home/john/Code/grail/src/grailbase/GrailPrefs.py,v $
 
 import os
