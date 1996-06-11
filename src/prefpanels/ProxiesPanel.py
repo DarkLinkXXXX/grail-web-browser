469134d715f0b5ec5c4afb8fd9b2f528cb8d808c
rmasse <rmasse>
1996-06-11 19:27:22 +0000

    Simplified the proxy panel controls to include less buttons... Fixing a bug along the way that left the panel in an ambigueous state.

--- src/prefpanels/ProxiesPanel.py
+++ src/prefpanels/ProxiesPanel.py
@@ -1,6 +1,6 @@
 """Grail proxy preferences panel."""
 
-__version__ = "$Revision: 1.5 $"
+__version__ = "$Revision: 1.6 $"
 # $Source: /home/john/Code/grail/src/prefpanels/ProxiesPanel.py,v $
 
 # Base class for the dialog:
@@ -30,15 +30,6 @@ class ProxiesPanel(PrefsPanels.Framework):
 	# Establish some booleans to represent the button states
 	self.no_proxy_enabled = IntVar()
 	self.manual_proxy_enabled = IntVar()
-	self.fb_state = BooleanVar()
-	self.hb_state = BooleanVar()
-	self.nb_state = BooleanVar()
-
-	#
-	# Set the Boolean vars to FALSE initially
-	self.fb_state.set(0)
-	self.hb_state.set(0)
-	self.nb_state.set(0)
 
 	#
 	# Create top level widgets
@@ -59,20 +50,14 @@ class ProxiesPanel(PrefsPanels.Framework):
 	       command=self.manual_switch)
 	
 	self.he = Entry(manual_right_frame, relief=SUNKEN, width=38)
-	self.hb = Checkbutton(manual_left_frame, relief=FLAT,
-	       variable=self.hb_state,
-	       text="HTTP Proxy (http://server:port):",
-	       command=self.http_entry_toggle)
+	self.hl = Label(manual_left_frame, relief=FLAT,
+	       text="HTTP Proxy (http://server:port):")
 	self.fe = Entry(manual_right_frame, relief=SUNKEN, width=38)
-	self.fb = Checkbutton(manual_left_frame, relief=FLAT,
-	       variable=self.fb_state,
-	       text=" FTP Proxy (http://server:port):",
-	       command=self.ftp_entry_toggle)
+	self.fl = Label(manual_left_frame, relief=FLAT,
+	       text=" FTP Proxy (http://server:port):",)
 
-	self.nb = Checkbutton(no_proxy_frame, relief=FLAT,
-	       variable=self.nb_state,
-	       text="Servers that need no proxy to be reached (www.python.org, www.dlib.org):",
-	       command=self.no_proxy_entry_toggle)
+	self.nl = Label(no_proxy_frame, relief=FLAT,
+	       text="Servers that need no proxy to be reached (www.python.org, .dlib.org):",)
 	self.ne = Entry(no_proxy_frame, relief=SUNKEN, width=75)
 
 	#
@@ -86,12 +71,12 @@ class ProxiesPanel(PrefsPanels.Framework):
 	no_proxy_frame.pack(side=TOP, expand=1, fill=X)
 	manual_right_frame.pack(side=RIGHT, expand=1, fill=X)
 	manual_left_frame.pack(side=LEFT, expand=1, fill=X)
-	self.nb.pack(side=TOP, expand=1, fill=X)
+	self.nl.pack(side=TOP, expand=1, fill=X)
 	self.ne.pack(side=BOTTOM, expand=1, fill=X)
 	self.he.pack(side=TOP, expand=1, fill=X)
-	self.hb.pack(side=TOP, expand=1, fill=X)
+	self.hl.pack(side=TOP, expand=1, fill=X)
 	self.fe.pack(side=TOP, expand=1, fill=X)
-	self.fb.pack(side=TOP, expand=1, fill=X)
+	self.fl.pack(side=TOP, expand=1, fill=X)
 
 	#
 	# Set the initial GUI state based on prefs
@@ -121,9 +106,6 @@ class ProxiesPanel(PrefsPanels.Framework):
 	    self.no_proxy_enabled.set(0)
 
 	self.manual_switch()
-	self.ftp_entry_toggle()
-	self.http_entry_toggle()
-	self.no_proxy_entry_toggle()
 	
 
     def register_prefs_UI(self):
@@ -147,16 +129,13 @@ class ProxiesPanel(PrefsPanels.Framework):
 
     def no_switch(self):
 	""" Set the state of the No Proxy Configuration controls
-	to DISABLED if the Checkbutton is no set."""
+	to DISABLED if the Checkbutton is not set."""
 
 	if self.no_proxy_enabled.get():
-	    self.nb_state.set(1)
-	    self.no.config(state=NORMAL)
-	    self.nb.config(state=NORMAL)
+	    self.nl.config(foreground='black')
 	    self.ne.config(state=NORMAL)
 	else:
-	    self.nb_state.set(0)
-	    self.nb.config(state=DISABLED)
+	    self.nl.config(foreground='grey')
 	    self.ne.config(state=DISABLED)
 	
     def manual_switch(self):
@@ -164,48 +143,22 @@ class ProxiesPanel(PrefsPanels.Framework):
 	to DISABLED if the Checkbutton is no set."""
 
 	if self.manual_proxy_enabled.get():
-	    self.hb.config(state=NORMAL)
-	    self.fb.config(state=NORMAL)
+	    self.hl.config(foreground='black')
+	    self.fl.config(foreground='black')
+	    self.he.config(state=NORMAL)
+	    self.fe.config(state=NORMAL)
 	    self.no.config(state=NORMAL)
 	    self.no_switch()
 	else:
-	    self.hb.config(state=DISABLED)
-	    self.fb.config(state=DISABLED)
+	    self.hl.config(foreground='grey')
+	    self.fl.config(foreground='grey')
+	    self.he.config(state=DISABLED)
+	    self.fe.config(state=DISABLED)
 	    #
 	    # We also disable No Proxy
 	    self.no_proxy_enabled.set(0)
-	    self.nb_state.set(0)
-	    self.nb.config(state=DISABLED)
+	    self.nl.config(foreground='grey')
 	    self.ne.config(state=DISABLED)
 	    self.no.config(state=DISABLED)
 	    
-	self.hb_state.set(0)
-	self.fb_state.set(0)
-	self.he.config(state=DISABLED)
-	self.fe.config(state=DISABLED)
 	    
-    def ftp_entry_toggle(self):
-	"""Enable and disable the FTP entry field."""
-
-	if self.fb_state.get():
-	    self.fe.config(state=NORMAL)
-	else:
-	    self.fe.config(state=DISABLED)
-	
-    def http_entry_toggle(self):
-	"""Enable and disable the HTTP entry field."""
-	
-	if self.hb_state.get():
-	    self.he.config(state=NORMAL)
-	else:
-	    self.he.config(state=DISABLED)
-	
-    def no_proxy_entry_toggle(self):
-	"""Enable and disable the No Proxy entry field."""
-
-	if self.nb_state.get():
-	    self.ne.config(state=NORMAL)
-	else:
-	    self.ne.config(state=DISABLED)
-
-
