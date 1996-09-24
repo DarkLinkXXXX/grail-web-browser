6487a91288296f1bb04758266064f7c1a0a30baf
fdrake <fdrake>
1996-09-24 15:20:18 +0000

    (grail.py):  Wrapped startup code in "if __name__ == '__main__':" statement; 	this allows grail.py to be byte-compiled.  A startup script can use 	this to shorten the startup time, very useful on old, slow machines! 	;-)
    	----------------------------------------------------------------
    	grail=/some/path/to/grail/grail.py
    	if [ \( ! -r ${grail}c \) -o $grail -nt ${grail}c ] ; then
    	    echo "Byte-compiling grail.py...."
    	    (cd `dirname $grail`;
    	     $python -c "import grail" >/dev/null 2>&1)
    	fi
    	grail=${grail}c
    
    	exec $python $grail ${1:+$@}

--- src/grail.py
+++ src/grail.py
@@ -711,18 +711,19 @@ def read_mime_types(file):
     return map
 
 
-if sys.argv[1:] and sys.argv[1][:2] == '-p':
-    p = sys.argv[1]
-    del sys.argv[1]
-    if p[2:]: n = eval(p[2:])
-    else: n = 20
-    KEEPALIVE_TIMER = 50000
-    import profile
-    profile.run('main()', '@grail.prof')
-    import pstats
-    p = pstats.Stats('@grail.prof')
-    p.strip_dirs().sort_stats('time').print_stats(n)
-    p.print_callers(n)
-    p.strip_dirs().sort_stats('cum').print_stats(n)
-else:
-    main()
+if __name__ == "__main__":
+    if sys.argv[1:] and sys.argv[1][:2] == '-p':
+	p = sys.argv[1]
+	del sys.argv[1]
+	if p[2:]: n = eval(p[2:])
+	else: n = 20
+	KEEPALIVE_TIMER = 50000
+	import profile
+	profile.run('main()', '@grail.prof')
+	import pstats
+	p = pstats.Stats('@grail.prof')
+	p.strip_dirs().sort_stats('time').print_stats(n)
+	p.print_callers(n)
+	p.strip_dirs().sort_stats('cum').print_stats(n)
+    else:
+	main()
