fe28fe1093526619667fc20b3acf57f8511b7b2c
fdrake <fdrake>
1997-05-30 21:39:32 +0000

    Make the <FN> tag be supported equally for both Tk and PostScript using the new intelligent device-generalization allowed by the extension mechanism.
    Simplifies the code and renames both handlers.

--- src/html/fn.py
+++ src/html/fn.py
@@ -6,7 +6,7 @@
 
 This supports the <FN ID=name> form of the footnote tag.
 """
-__version__ = '$Revision: 2.4 $'
+__version__ = '$Revision: 2.5 $'
 #  $Source: /home/john/Code/grail/src/html/fn.py,v $
 
 
@@ -15,17 +15,15 @@ ATTRIBUTES_AS_KEYWORDS = 1
 from grailutil import extract_keyword
 
 
-def start_fn(parser, attrs):
+def writer_start_fn(parser, attrs):
     if 'p' in parser.stack:
 	parser.lex_endtag('p')
 	parser.formatter.end_paragraph(0)
     else:
 	parser.formatter.add_line_break()
-    name = extract_keyword('id', attrs)
-    parser.formatter.push_style(name and '#' + name or None)
 
 
-def end_fn(parser):
+def writer_end_fn(parser):
     parser.formatter.pop_style()
 
 
