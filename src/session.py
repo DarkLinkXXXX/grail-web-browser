7272e317c6b5d63c949dc82b61366298f2deceeb
fdrake <fdrake>
1998-07-20 22:57:44 +0000

    Do the right stuff for session management.

---
+++ src/session.py
@@ -0,0 +1,56 @@
+"""Client-side session management support."""
+__version__ = '$Revision: 2.1 $'
+
+import socket
+import string
+import sys
+
+
+class Session:
+    def __init__(self, command=None, client=None):
+        self.__master = None
+        self.__windows = []
+        #
+        if command is None:
+            command = [sys.executable]
+            if __debug__:
+                command.append("-O")
+            command = command + sys.argv
+            command = string.join(command)
+        if client is None:
+            hostname = socket.gethostname()
+            client = socket.gethostbyaddr(socket.gethostbyname(hostname))[0]
+        #
+        self.set_command(command)
+        self.set_client(client)
+
+    def set_command(self, command):
+        self.__command = command
+        for window in self.__windows:
+            window.command(command)
+
+    def set_client(self, client):
+        self.__client = client
+        for window in self.__windows:
+            window.client(client)
+
+    def add_window(self, window):
+        self.__windows.append(window)
+        if self.__master is None:
+            self.__set_master()
+        self.__master.group(window)
+
+    def del_window(self, window):
+        if window is self.__master:
+            self.__master = None
+        try: self.__windows.remove(window)
+        except ValueError: pass
+        if self.__windows and self.master is None:
+            self.__set_master()
+
+    def __set_master(self):
+        self.__master = self.__windows[0]
+        self.__master.command(self.__command)
+        self.__master.client(self.__client)
+        for window in self.__windows:
+            self.__master.group(window)
