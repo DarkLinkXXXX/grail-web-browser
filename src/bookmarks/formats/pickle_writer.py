674cd3ed9434bc6c5f25dc0efe496fbe295da8bb
fdrake <fdrake>
1998-10-23 21:50:27 +0000

    Small adjustments to better conform to the common Writer interface.

--- src/bookmarks/formats/pickle_writer.py
+++ src/bookmarks/formats/pickle_writer.py
@@ -1,6 +1,6 @@
 """Writer for Grail's pickled bookmarks."""
 
-__version__ = '$Revision: 1.1 $'
+__version__ = '$Revision: 1.2 $'
 
 
 import bookmarks                        # parent
@@ -20,17 +20,20 @@ class Writer(bookmarks.BookmarkWriter):
     __filename = ""
     __mtime = 0
 
+    def __init__(self, root):
+        self.__root = root
+
     def set_original_filename(self, filename):
         self.__filename = filename
 
     def set_original_mtime(self, mtime):
         self.__mtime = mtime
 
-    def write_tree(self, root, fp):
+    def write_tree(self, fp):
         try:
             fp.write(self.HEADER_STRING)
             fp.write(self.__filename + "\n")
             fp.write(`self.__mtime` + "\n")
-            pickle.dump(root, fp, 1)
+            pickle.dump(self.__root, fp, 1)
         finally:
             fp.close()
