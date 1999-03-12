ba17b3d5a08a2e21ff47ec88b557c6a056d3c830
fdrake <fdrake>
1999-03-12 17:13:12 +0000

    Fix up the general parsing routines a bit.
    Clean up some of the internal class management.

--- src/bookmarks/xmlinfo.py
+++ src/bookmarks/xmlinfo.py
@@ -8,7 +8,7 @@ There are parts of this module which assume the native character encoding is
 ASCII or a superset; this should be fixed.
 """
 
-__version__ = "$Revision: 1.3 $"
+__version__ = "$Revision: 1.4 $"
 
 import copy
 import os
@@ -66,7 +66,7 @@ class Record:
 
 
 def get_xml_info(buffer):
-    values = Record(standalone="no", xml_version="1.0")
+    values = Record()
     byte_order, encoding, bom = guess_byte_order_and_encoding(buffer)
     values.byte_order = byte_order
     values.encoding = encoding
@@ -148,9 +148,13 @@ def new_extractor(encoding, buffer, values):
     klass = _extractor_map.get(encoding, Extractor)
     return klass(buffer, values)
 
+def add_extractor_class(klass):
+    for enc in klass.Encodings:
+        _extractor_map[enc] = klass
+
 
 class Extractor:
-    __VERSION_CHARS = string.letters + string.digits + "_.:-"
+    VERSION_CHARS = string.letters + string.digits + "_.:-"
 
     encodings = ()
 
@@ -198,8 +202,9 @@ class Extractor:
                "first pseudo-attribute in XML declaration must be version")
         if not verno:
             raise ParseError("version number cannot be empty")
+        version_chars = self.VERSION_CHARS
         for c in verno:
-            if not c in self.__VERSION_CHARS:
+            if not c in version_chars:
                 raise ParseError(
                     "illegal character in XML version declaration: " + `c`)
         self.values.xml_version = verno
@@ -261,78 +266,117 @@ class Extractor:
             d[o] = o
         return d.has_key
 
-    BASE_CHARS = (range(0x41, 0x5A+1) + range(0x61, 0x7A+1)
-                  + range(0xC0, 0xD6+1) + range(0xD8, 0xF6+1)
-                  + range(0xF8, 0xFF+1) + range(0x100, 0x131+1)
-                  + range(0x134, 0x13E+1) + range(0x141, 0x148+1)
-                  + range(0x14A, 0x17E+1) + range(0x180, 0x1C3+1)
-                  + range(0x1CD, 0x1F0+1) + range(0x1F4, 0x1F5+1)
-                  + range(0x1FA, 0x217+1) + range(0x250, 0x2A8+1)
-                  + range(0x2BB, 0x2C1+1) + [0x386]
-                  + range(0x388, 0x38A+1) + [0x38C]
-                  + range(0x38E, 0x3A1+1) + range(0x3A3, 0x3CE+1)
-                  + range(0x3D0, 0x3D6+1) + [0x3DA, 0x3DC, 0x3DE, 0x3E0]
-                  + range(0x3E2, 0x3F3+1) + range(0x401, 0x40C+1)
-                  + range(0x40E, 0x44F+1) + range(0x451, 0x45C+1)
-                  + range(0x45E, 0x481+1) + range(0x490, 0x4C4+1)
-                  + range(0x4C7, 0x4C8+1) + range(0x4CB, 0x4CC+1)
-                  + range(0x4D0, 0x4EB+1) + range(0x4EE, 0x4F5+1)
-                  + range(0x4F8, 0x4F9+1) + range(0x531, 0x556+1)
-                  + [0x559]
-                  + range(0x561, 0x586+1) + range(0x5D0, 0x5EA+1)
-                  + range(0x5F0, 0x5F2+1) + range(0x621, 0x63A+1)
-                  + range(0x641, 0x64A+1) + range(0x905, 0x939+1)
-                  + [0x93D]
-                  + range(0x9AA, 0x9B0+1) + [0x9B2]
-                  + range(0xA05, 0xA0A+1) + range(0xA35, 0xA36+1)
-                  + range(0xA8F, 0xA91+1) + [0xAE0]
-                  + range(0xB05, 0xB0C+1) + range(0xB36, 0xB39+1)
-                  + [0xB3D]
-                  + range(0xB5F, 0xB61+1) + range(0xB85, 0xB8A+1)
-                  + range(0xB8E, 0xB90+1) + range(0xB92, 0xB95+1)
-                  + range(0xB99, 0xB9A+1) + [0xB9C]
-                  + range(0xB9E, 0xB9F+1) + range(0xBA3, 0xBA4+1)
-                  + range(0xBA8, 0xBAA+1) + range(0xBAE, 0xBB5+1)
-                  + range(0xBB7, 0xBB9+1) + range(0xC05, 0xC0C+1)
-                  + range(0xC0E, 0xC10+1) + range(0x10A0, 0x10C5+1)
-                  + range(0x10D0, 0x10F6+1) + [0x1100]
-                  + range(0x1102, 0x1103+1) + range(0x1105, 0x1107+1)
-                  + [0x1109]
-                  + range(0x110B, 0x110C+1) + range(0x110E, 0x1112+1)
-                  + [0x113C, 0x113E, 0x1140, 0x114C, 0x114E, 0x1150]
-                  + range(0x1154, 0x1155+1) + [0x1159]
-                  + range(0x115F, 0x1161+1) + [0x1163, 0x1165, 0x1167, 0x1169]
-                  + range(0x116D, 0x116E+1) + range(0x1172, 0x1173+1)
-                  + [0x1175, 0x119E, 0x11A8, 0x11AB]
-                  + range(0x1F5F, 0x1F7D+1) + range(0x1F80, 0x1FB4+1)
-                  + range(0x1FB6, 0x1FBC+1) + [0x1FBE]
-                  + range(0x1FC2, 0x1FC4+1) + range(0x1FC6, 0x1FCC+1)
-                  + range(0x1FD0, 0x1FD3+1))
+    BASE_CHARS = tuple(
+        range(0x41, 0x5A+1) + range(0x61, 0x7A+1)
+        + range(0xC0, 0xD6+1) + range(0xD8, 0xF6+1)
+        + range(0xF8, 0xFF+1) + range(0x100, 0x131+1)
+        + range(0x134, 0x13E+1) + range(0x141, 0x148+1)
+        + range(0x14A, 0x17E+1) + range(0x180, 0x1C3+1)
+        + range(0x1CD, 0x1F0+1) + range(0x1F4, 0x1F5+1)
+        + range(0x1FA, 0x217+1) + range(0x250, 0x2A8+1)
+        + range(0x2BB, 0x2C1+1) + [0x386]
+        + range(0x388, 0x38A+1) + [0x38C]
+        + range(0x38E, 0x3A1+1) + range(0x3A3, 0x3CE+1)
+        + range(0x3D0, 0x3D6+1) + [0x3DA, 0x3DC, 0x3DE, 0x3E0]
+        + range(0x3E2, 0x3F3+1) + range(0x401, 0x40C+1)
+        + range(0x40E, 0x44F+1) + range(0x451, 0x45C+1)
+        + range(0x45E, 0x481+1) + range(0x490, 0x4C4+1)
+        + range(0x4C7, 0x4C8+1) + range(0x4CB, 0x4CC+1)
+        + range(0x4D0, 0x4EB+1) + range(0x4EE, 0x4F5+1)
+        + range(0x4F8, 0x4F9+1) + range(0x531, 0x556+1) + [0x559]
+        + range(0x561, 0x586+1) + range(0x5D0, 0x5EA+1)
+        + range(0x5F0, 0x5F2+1) + range(0x621, 0x63A+1)
+        + range(0x641, 0x64A+1) + range(0x905, 0x939+1) + [0x93D]
+        + range(0x9AA, 0x9B0+1) + [0x9B2]
+        + range(0xA05, 0xA0A+1) + range(0xA35, 0xA36+1)
+        + range(0xA8F, 0xA91+1) + [0xAE0]
+        + range(0xB05, 0xB0C+1) + range(0xB36, 0xB39+1) + [0xB3D]
+        + range(0xB5F, 0xB61+1) + range(0xB85, 0xB8A+1)
+        + range(0xB8E, 0xB90+1) + range(0xB92, 0xB95+1)
+        + range(0xB99, 0xB9A+1) + [0xB9C]
+        + range(0xB9E, 0xB9F+1) + range(0xBA3, 0xBA4+1)
+        + range(0xBA8, 0xBAA+1) + range(0xBAE, 0xBB5+1)
+        + range(0xBB7, 0xBB9+1) + range(0xC05, 0xC0C+1)
+        + range(0xC0E, 0xC10+1) + range(0x10A0, 0x10C5+1)
+        + range(0x10D0, 0x10F6+1) + [0x1100]
+        + range(0x1102, 0x1103+1) + range(0x1105, 0x1107+1) + [0x1109]
+        + range(0x110B, 0x110C+1) + range(0x110E, 0x1112+1)
+        + [0x113C, 0x113E, 0x1140, 0x114C, 0x114E, 0x1150]
+        + range(0x1154, 0x1155+1) + [0x1159]
+        + range(0x115F, 0x1161+1) + [0x1163, 0x1165, 0x1167, 0x1169]
+        + range(0x116D, 0x116E+1) + range(0x1172, 0x1173+1)
+        + [0x1175, 0x119E, 0x11A8, 0x11AB]
+        + range(0x1F5F, 0x1F7D+1) + range(0x1F80, 0x1FB4+1)
+        + range(0x1FB6, 0x1FBC+1) + [0x1FBE]
+        + range(0x1FC2, 0x1FC4+1) + range(0x1FC6, 0x1FCC+1)
+        + range(0x1FD0, 0x1FD3+1))
 
-    COMBINING_CHARS = []
+    COMBINING_CHARS = tuple(
+        range(0x300, 0x345+1) + range(0x360, 0x361+1)
+        + range(0x483, 0x486+1) + range(0x591, 0x5A1+1)
+        + range(0x5A3, 0x5B9+1) + range(0x5BB, 0x5BD+1) + [0x5BF]
+        + range(0x5C1, 0x5C2+1) + [0x5C4]
+        + range(0x64B, 0x652+1) + [0x670]
+        + range(0x6D6, 0x6DC+1) + range(0x6DD, 0x6DF+1)
+        + range(0x6E0, 0x6E4+1) + range(0x6E7, 0x6E8+1)
+        + range(0x6EA, 0x6ED+1) + range(0x901, 0x903+1) + [0x93C]
+        + range(0x93E, 0x94C+1) + [0x94D]
+        + range(0x951, 0x954+1) + range(0x962, 0x963+1)
+        + range(0x981, 0x983+1) + [0x9BC, 0x9BE, 0x9BF]
+        + range(0x9C0, 0x9C4+1) + range(0x9C7, 0x9C8+1)
+        + range(0x9CB, 0x9CD+1) + [0x9D7]
+        + range(0x9E2, 0x9E3+1) + [0xA02, 0xA3C, 0xA3E, 0xA3F]
+        + range(0xA40, 0xA42+1) + range(0xA47, 0xA48+1)
+        + range(0xA4B, 0xA4D+1) + range(0xA70, 0xA71+1)
+        + range(0xA81, 0xA83+1) + [0xABC]
+        + range(0xABE, 0xAC5+1) + range(0xAC7, 0xAC9+1)
+        + range(0xACB, 0xACD+1) + range(0xB01, 0xB03+1) + [0xB3C]
+        + range(0xB3E, 0xB43+1) + range(0xB47, 0xB48+1)
+        + range(0xB4B, 0xB4D+1) + range(0xB56, 0xB57+1)
+        + range(0xB82, 0xB83+1) + range(0xBBE, 0xBC2+1)
+        + range(0xBC6, 0xBC8+1) + range(0xBCA, 0xBCD+1) + [0xBD7]
+        + range(0xC01, 0xC03+1) + range(0xC3E, 0xC44+1)
+        + range(0xC46, 0xC48+1) + range(0xC4A, 0xC4D+1)
+        + range(0xC55, 0xC56+1) + range(0xC82, 0xC83+1)
+        + range(0xCBE, 0xCC4+1) + range(0xCC6, 0xCC8+1)
+        + range(0xCCA, 0xCCD+1) + range(0xCD5, 0xCD6+1)
+        + range(0xD02, 0xD03+1) + range(0xD3E, 0xD43+1)
+        + range(0xD46, 0xD48+1) + range(0xD4A, 0xD4D+1) + [0xD57, 0xE31]
+        + range(0xE34, 0xE3A+1) + range(0xE47, 0xE4E+1) + [0xEB1]
+        + range(0xEB4, 0xEB9+1) + range(0xEBB, 0xEBC+1)
+        + range(0xEC8, 0xECD+1) + range(0xF18, 0xF19+1)
+        + [0xF35, 0xF37, 0xF39, 0xF3E, 0xF3F]
+        + range(0xF71, 0xF84+1) + range(0xF86, 0xF8B+1)
+        + range(0xF90, 0xF95+1) + [0xF97]
+        + range(0xF99, 0xFAD+1) + range(0xFB1, 0xFB7+1) + [0xFB9]
+        + range(0x20D0, 0x20DC+1) + [0x20E1]
+        + range(0x302A, 0x302F+1) + [0x3099, 0x309A])
 
-    DIGIT_CHARS = (range(0x30, 0x3A+1) + range(0x660, 0x669+1)
-                   + range(0x6F0, 0x6F9+1) + range(0x966, 0x96F+1)
-                   + range(0x9E6, 0x9EF+1) + range(0xA66, 0xA6F+1)
-                   + range(0xAE6, 0xAEF+1) + range(0xB66, 0xB6F+1)
-                   + range(0xBE7, 0xBEF+1) + range(0xC66, 0xC6F+1)
-                   + range(0xCE6, 0xCEF+1) + range(0xD66, 0xD6F+1)
-                   + range(0xE50, 0xE59+1) + range(0xED0, 0xED9+1)
-                   + range(0xF20, 0xF29+1))
+    DIGIT_CHARS = tuple(
+        range(0x30, 0x3A+1) + range(0x660, 0x669+1)
+        + range(0x6F0, 0x6F9+1) + range(0x966, 0x96F+1)
+        + range(0x9E6, 0x9EF+1) + range(0xA66, 0xA6F+1)
+        + range(0xAE6, 0xAEF+1) + range(0xB66, 0xB6F+1)
+        + range(0xBE7, 0xBEF+1) + range(0xC66, 0xC6F+1)
+        + range(0xCE6, 0xCEF+1) + range(0xD66, 0xD6F+1)
+        + range(0xE50, 0xE59+1) + range(0xED0, 0xED9+1)
+        + range(0xF20, 0xF29+1))
     is_digit_char = _make_set_predicate(DIGIT_CHARS)
 
-    EXTENDING_CHARS = ([0xB7, 0x2D0, 0x2D1, 0x387, 0x640, 0xE46, 0xEC6,
-                        0x3005] + range(0x3031, 0x3035+1)
-                       + range(0x309D, 0x309E+1) + range(0x30FC, 0x30FE+1))
+    EXTENDING_CHARS = tuple(
+        [0xB7, 0x2D0, 0x2D1, 0x387, 0x640, 0xE46, 0xEC6, 0x3005]
+        + range(0x3031, 0x3035+1) + range(0x309D, 0x309E+1)
+        + range(0x30FC, 0x30FE+1))
     is_extending_char = _make_set_predicate(EXTENDING_CHARS)
 
-    IDEOGRAPHIC_CHARS = range(0x4E00, 0x9FA5+1) + range(0x3021, 0x3029+1)
+    IDEOGRAPHIC_CHARS = tuple(
+        range(0x4E00, 0x9FA5+1) + range(0x3021, 0x3029+1))
     is_ideographic_char = _make_set_predicate(IDEOGRAPHIC_CHARS)
 
     LETTER_CHARS = BASE_CHARS + IDEOGRAPHIC_CHARS
     is_letter_char = _make_set_predicate(LETTER_CHARS)
 
-    NAME_CHARS = LETTER_CHARS + DIGIT_CHARS + [46, 45, 95, 58] \
+    NAME_CHARS = LETTER_CHARS + DIGIT_CHARS + (46, 45, 95, 58) \
                  + COMBINING_CHARS + EXTENDING_CHARS
     is_name_char = _make_set_predicate(NAME_CHARS)
 
@@ -443,15 +487,16 @@ class Extractor:
         count = 0
         try:
             while 1:
-                c = self.get_ascii(1)
+                c, u = self.get_char_and_unicode(count)
                 if not c:
                     break
-                if ord(c) not in (0x9, 0xA, 0xD, 0x20):
+                if u not in (0x9, 0xA, 0xD, 0x20):
                     break
-                self.discard_chars(1)
                 count = count + 1
         except ConversionError:
             pass
+        if count:
+            self.discard_chars(count)
         return count
 
     def require_whitespace(self, where):
@@ -496,9 +541,6 @@ class Extractor:
                     self.discard_chars(width)
 
     def discard_chars(self, count):
-        raise NotImplementedError
-
-    def lower(self, str):
         raise NotImplementedError
 
 
@@ -584,29 +626,46 @@ class ISO8859Extractor(Extractor):
         return string.lower(str)
 
 
-for c in "123456789":
+class ISO8859_1_Extractor(ISO8859Extractor):
+    Encodings = ("iso-8859-1", "iso-latin-1", "latin-1")
+
+    def get_ascii(self, count):
+        return self.buffer[:count]
+
+    def get_char_and_unicode(self, index=0):
+        c = self.buffer[index:index + 1]
+        if c:
+            return c, ord(c)
+        else:
+            return c, None
+
+add_extractor_class(ISO8859_1_Extractor)
+
+
+for c in "23456789":
     class _Extractor(ISO8859Extractor):
-        encodings = ("iso-8859-" + c,)
+        Encodings = ("iso-8859-" + c,)
     try:
         _Extractor.__name__ = "ISO8859_%s_Extractor" % c
     except TypeError:
         # older Python versions wouldn't allow __name__ to be set on a class
         pass
     exec "ISO8859_%s_Extractor = _Extractor" % c
-    _extractor_map["iso-8859-" + c] = _Extractor
+    add_extractor_class(_Extractor)
+del _Extractor
 
 
 class UTF8Extractor(ISO8859Extractor):
-    encodings = ("utf-8",)
+    Encodings = ("utf-8",)
 
     def get_char_and_unicode(self, index=0):
         raise NotImplementedError
 
-_extractor_map["utf-8"] = UTF8Extractor
+add_extractor_class(UTF8Extractor)
 
 
 class EBCDICExtractor(Extractor):
-    encodings = ("ebcdic",)
+    Encodings = ("ebcdic",)
 
     # This table was taken from the source code of GNU recode 3.4.
     __ASCII_TO_EBCDIC = [
@@ -662,7 +721,7 @@ class EBCDICExtractor(Extractor):
         buffer = self.buffer[:count]
         return string.translate(buffer, self.__translation)
 
-_extractor_map['ebcdic'] = EBCDICExtractor
+add_extractor_class(EBCDICExtractor)
 
 
 def ascii_to_ucs2be(s):
@@ -690,7 +749,7 @@ def ascii_to_ucs4le(s):
 
 
 class UCS2Extractor(Extractor):
-    encodings = ("ucs-2", "utf-16", "iso-10646-ucs-2")
+    Encodings = ("ucs-2", "utf-16", "iso-10646-ucs-2")
 
     __WHITESPACE_BE = map(ascii_to_ucs2be, string.whitespace)
     __WHITESPACE_LE = map(ascii_to_ucs2le, string.whitespace)
@@ -748,8 +807,7 @@ class UCS2Extractor(Extractor):
     def discard_chars(self, count):
         self.buffer = self.buffer[count*2:]
 
-_extractor_map["ucs-2"] = UCS2Extractor
-_extractor_map["utf-16"] = UCS2Extractor
+add_extractor_class(UCS2Extractor)
 
 
 def ordwc(wc, byte_order=None):
