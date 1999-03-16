f0756e9292ba8f549cb8a60731d307695bbbd722
fdrake <fdrake>
1999-03-16 23:44:03 +0000

    Ok, so conv_mimetype() should be here as well.

--- src/sgml/utils.py
+++ src/sgml/utils.py
@@ -3,7 +3,7 @@
 This module is safe for 'from sgml.utils import *'.
 
 """
-__version__ = '$Revision: 1.1 $'
+__version__ = '$Revision: 1.2 $'
 
 
 import string
@@ -33,6 +33,7 @@ def extract_attribute(key, dict, default=None, conv=None, delete=1):
             return default
     return default
 
+
 def extract_keyword(key, dict, default=None, conv=None):
     """Extracts an attribute from a dictionary.
 
@@ -49,6 +50,7 @@ def extract_keyword(key, dict, default=None, conv=None):
         return dict[key]
     return default
 
+
 def conv_integer(val, conv=_string.atoi, otherlegal=''):
     val = _string.strip(val)
     l = len(val)
@@ -62,11 +64,14 @@ def conv_integer(val, conv=_string.atoi, otherlegal=''):
             break
     return conv(val)
 
+
 def conv_float(val):
     return conv_integer(val, conv=_string.atof, otherlegal='.')
 
+
 def conv_normstring(val):
     return _string.lower(_string.strip(val))
+
 
 def conv_enumeration(val, mapping_or_list):
     val = conv_normstring(val)
@@ -77,8 +82,49 @@ def conv_enumeration(val, mapping_or_list):
         if mapping_or_list.has_key(val): return mapping_or_list[val]
         else: return None
 
+
 def conv_normwhitespace(val):
     return _string.join(_string.split(val))
 
+
 def conv_exists(val):
     return 1
+
+
+def conv_mimetype(type):
+    """Convert MIME media type specifications to tuples of
+    ('type/subtype', {'option': 'value'}).
+    """
+    if not type:
+        return None, {}
+    if ';' in type:
+        i = _string.index(type, ';')
+        opts = _parse_mimetypeoptions(type[i + 1:])
+        type = type[:i]
+    else:
+        opts = {}
+    fields = _string.split(_string.lower(type), '/')
+    if len(fields) != 2:
+        raise ValueError, "Illegal media type specification."
+    type = _string.join(fields, '/')
+    return type, opts
+
+
+def _parse_mimetypeoptions(options):
+    opts = {}
+    options = _string.strip(options)
+    while options:
+        if '=' in options:
+            pos = _string.find(options, '=')
+            name = _string.lower(_string.strip(options[:pos]))
+            value = _string.strip(options[pos + 1:])
+            options = ''
+            if ';' in value:
+                pos = _string.find(value, ';')
+                options = _string.strip(value[pos + 1:])
+                value = _string.strip(value[:pos])
+            if name:
+                opts[name] = value
+        else:
+            options = None
+    return opts
