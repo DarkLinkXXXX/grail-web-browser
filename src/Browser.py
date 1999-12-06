18121b50e47613ee3c170673425871b238b966bd
fdrake <fdrake>
1999-12-06 17:11:44 +0000

    When creating a new Browser window, set focus to the viewer's text widget so that page up & page down work right away, without the user having to click on the text widget.

--- src/Browser.py
+++ src/Browser.py
@@ -87,6 +87,7 @@ class Browser:
         self.create_statusbar()
         self.viewer = Viewer(self.root, browser=self,
                              width=width, height=height)
+        self.viewer.text.focus_set()
         self.context = self.viewer.context
         if self.app.prefs.GetBoolean('browser', 'show-logo'):
             self.logo_init()
